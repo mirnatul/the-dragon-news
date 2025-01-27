066d98e08327929b6e0cf0f4a96211db5fd98579
mirnatul <www.mirnatul@gmail.com>
2025-01-27 17:52:17 +0600

    left side navigation done, ready for update

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 const Root = () => {
     return (
         <div className='bg-white'>
-            <div className='max-w-6xl mx-auto font-poppins'>
+            <div className='max-w-7xl mx-auto font-poppins'>
                 <Outlet></Outlet>
             </div>
         </div>
