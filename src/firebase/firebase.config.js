22c0790e23caf1991de71edbdd78c9f460462f4d
mirnatul <www.mirnatul@gmail.com>
2025-01-28 18:38:24 +0600

    env file applied

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,15 +2,15 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+console.log(import.meta.env.VITE_PASS);
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAXlyIDypDu34I10l3SLgQYicuV_NXZuNw",
-    authDomain: "the-news-dragon-586a4.firebaseapp.com",
-    projectId: "the-news-dragon-586a4",
-    storageBucket: "the-news-dragon-586a4.firebasestorage.app",
-    messagingSenderId: "999116415735",
-    appId: "1:999116415735:web:314b4c2bae35ac1ba5a9f2"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
